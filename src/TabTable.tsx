e696c913e7aba17f66f3bf250ee39f2a5b6614b4
Mark De Souza <markcdesouza@gmail.com>
2023-03-27 11:20:53 +1100

    Switched to div based table Added close tab functionality Added switch to tab functionality

--- src/TabTable.tsx
+++ src/TabTable.tsx
@@ -1,4 +1,4 @@
-import { faSort, faSortAsc, faSortDesc } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faSort, faSortAsc, faSortDesc, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
 
@@ -14,39 +14,48 @@ function TabTable(props: TabTableProps) {
     const [sortAsc, setSortAsc] = useState<boolean>(true);
 
     useEffect(() => {
+        updateTabList();
+    }, []);
+
+    function updateTabList() {
         let queryOptions = {};
         chrome.tabs.query(queryOptions, tabs => {
             setTabs(tabs);
             props.onCount(tabs.length);
         });
-    }, []);
+    }
 
-
-    const headers = ["Title", "URL"];
+    const headers = ["", "", "Title", "URL"];
     const headersColumns = headers.map((header) => {
-            if (header == "") {
-                return (<th scope="col" />);
-            }
-
-            var sortIcon, clickHandler;
-            if (sortIndex === header) {
-                if (sortAsc === true) {
-                    sortIcon = faSortAsc;
-                    clickHandler = () => { setSortIndex(header); setSortAsc(false); }
-                } else {
-                    sortIcon = faSortDesc;
-                    clickHandler = () => { setSortIndex(""); setSortAsc(true); }
-                }
-            } else {
-                sortIcon = faSort;
-                clickHandler = () => { setSortIndex(header); setSortAsc(true); }
-            }
-
-            return (<th scope="col" onClick={clickHandler} >{header} <FontAwesomeIcon icon={sortIcon} /></th>)
+        if (header == "") {
+            return (<div>&nbsp;</div>);
         }
+
+        var sortIcon, sortTitle, sortCss, clickHandler;
+        if (sortIndex === header) {
+            if (sortAsc === true) {
+                sortIcon = faSortAsc;
+                sortTitle = "Sort Descending";
+                sortCss = "";
+                clickHandler = () => { setSortIndex(header); setSortAsc(false); }
+            } else {
+                sortIcon = faSortDesc;
+                sortTitle = "Default Sorting";
+                sortCss = "";
+                clickHandler = () => { setSortIndex(""); setSortAsc(true); }
+            }
+        } else {
+            sortIcon = faSort;
+            sortTitle = "Sort Ascending";
+            sortCss = "inactiveSort";
+            clickHandler = () => { setSortIndex(header); setSortAsc(true); }
+        }
+
+        return (<div onClick={clickHandler} className="tabTableHeader">{header} <FontAwesomeIcon icon={sortIcon} title={sortTitle} className={sortCss} /></div>)
+    }
     );
 
-    const compareTabs = (a:chrome.tabs.Tab, b:chrome.tabs.Tab) => {
+    const compareTabs = (a: chrome.tabs.Tab, b: chrome.tabs.Tab) => {
         if (sortIndex == "") {
             return 0;
         }
@@ -69,33 +78,55 @@ function TabTable(props: TabTableProps) {
 
     const search = props.filter.toLowerCase();
     const dataRows = tabs.filter(
-        tab => ((search === "") || 
-                (tab.title?.toLowerCase().indexOf(search) !== -1) || 
-                (tab.url?.toLowerCase().indexOf(search) !== -1))).sort(compareTabs).map((tab) => 
-        <TabRow tab={tab} key={tab.id} />
-    );
-        tabs.sort()
+        tab => ((search === "") ||
+            (tab.title?.toLowerCase().indexOf(search) !== -1) ||
+            (tab.url?.toLowerCase().indexOf(search) !== -1))).sort(compareTabs).map((tab) =>
+                <TabRow tab={tab} updateTabList={updateTabList} key={tab.id} />
+            );
+    tabs.sort()
     return (
-        <table className="tabTable">
-            <tr>
+        <div className="tabTable">
+            <div className="tabTableRow">
                 {headersColumns}
-            </tr>
+            </div>
             {dataRows}
-        </table>
+        </div>
     );
 }
 
 
 interface TabRowProps {
     tab: chrome.tabs.Tab
+    updateTabList: Function
 }
 
 function TabRow(props: TabRowProps) {
+
+    function switchToTab() {
+        if (props.tab.windowId) {
+            chrome.windows.update(props.tab.windowId, {focused: true}, (window) => {
+                if (props.tab.id) {
+                    chrome.tabs.update(props.tab.id, { active: true });
+                }
+            })
+        }
+    }
+
+    function closeTab() {
+        if (props.tab.id) {
+            chrome.tabs.remove(props.tab.id, () => {
+                props.updateTabList();
+            });
+        }
+    }
+
     return (
-        <tr>
-            <td>{props.tab.title}</td>
-            <td>{props.tab.url}</td>
-        </tr>
+        <div className="tabTableRow">
+            <div><FontAwesomeIcon onClick={switchToTab} icon={faEye} className="tabActionIcon" title="Switch to Tab" /></div>
+            <div><FontAwesomeIcon onClick={closeTab} icon={faTrashCan} className="tabActionIcon" title="Close Tab" /></div>
+            <div>{props.tab.title}</div>
+            <div>{props.tab.url}</div>
+        </div>
     );
 }
 
