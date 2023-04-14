402060b47c527de81280e093eba91a209a4630ee
Mark De Souza <markcdesouza@gmail.com>
2023-04-15 08:17:34 +1000

    - Management tab is now a singleton

--- public/popup.js
+++ public/popup.js
@@ -1,4 +1,22 @@
 const url = chrome.runtime.getURL("index.html");
-chrome.tabs.create({ url: url }, () => {
-  window.close();
+chrome.tabs.query({ url: url }, (tabs) => {
+  // Find if an tab of tab manager is already open
+  for (const tab of tabs) {
+    if (tab.windowId) {
+      chrome.windows.getCurrent((currentWindow) => {
+        if (currentWindow.id !== tab.windowId) {
+          chrome.windows.update(tab.windowId, { focused: true }, (activeWindow) => {
+            chrome.tabs.update(tab.id, { active: true });
+          });
+        } else {
+          chrome.tabs.update(tab.id, { active: true });
+        }
+      });
+      return;
+    }
+    // If not, open a new tab
+    chrome.tabs.create({ url: url }, () => {
+      window.close();
+    });
+  }
 });
